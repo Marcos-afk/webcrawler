af902f899db0d487e2c153c1cbfede049722acc8
Kaleb Carvalho <kalebcarvalho1@gmail.com>
2023-05-25 17:56:13 -0300

    Update main.py
    Adicionados métodos do scrape

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
-from func import *
+from functions import *
 
-url = "https://www.youtube.com"
+urls = []  # Lista de links
+documentos = []  # Lista de listas dos termos de cada site
+all_termos = []  # Todos os vocabulários em uma única lista
 
-html = get_html(url)
-head_tag = get_head(url)
-body_tag = get_body(url)
-meta_tags = get_meta(url)
-internos, externos = get_links(url)
-termos = get_terms(url)
+# Cria lista baseada no arquivo de links
+with open("websites.txt", "r") as f:
+    lines = f.readlines()
+    for i in range(len(lines)):
+        urls.append(lines[i].strip())
 
-vocabulario = vocabulary(termos)
+# Loop da lista de links
+for url in urls:
+    try:
+        response = requests.get(url)
+        # Se o site aceitar o pedido:
+        if response.status_code == 200:
+            # Cria o nome do banco de dados a partir da URL atual
+            name = urlparse(url).netloc
+            new_name = name.replace('.', '-')
+
+            # Adquire todos os dados
+            html = get_html(response)
+            head_tag = get_head(response)
+            body_tag = get_body(response)
+            meta_tags = get_meta(response)
+            internos, externos = get_links(url)
+            termos = get_terms(response)
+            vocabulario = vocabulary(termos)
+
+            # Cria dados para matriz
+            for termo in vocabulario:
+                all_termos.append(termo)
+
+            documentos.append(vocabulario)
+
+            # Insere todos os dados no banco
+            insert_data(new_name, "HTML", str(html))
+            insert_data(new_name, "Head tags", head_tag)
+            insert_data(new_name, "Body tags", body_tag)
+            insert_data(new_name, "Meta tags", meta_tags)
+            if len(internos) > 0:
+                insert_data(new_name, "Links internos", list(internos))
+            if len(externos) > 0:
+                insert_data(new_name, "Links externos", list(externos))
+            insert_data(new_name, "termos", termos)
+            insert_data(new_name, "vocabulario", vocabulario)
+
+    # Se não houver resposta, continua com o próximo link na lista
+    except requests.exceptions.RequestException:
+        continue
+
+# Cria a matriz e insere no banco
+matriz = matrix(all_termos, documentos)
+insert_matrix("Matriz", "Booleana", matriz)
